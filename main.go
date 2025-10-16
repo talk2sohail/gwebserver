6c37f858d5d678b04f29054c9302c596c30efc1e
Md Sohail <mdsohail@Mds-MacBook-Air.local>
2025-10-16 23:06:03 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,6 +16,31 @@ func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("method=%s uri=%s remote_addr=%s user_agent=%s", r.Method, r.RequestURI, r.RemoteAddr, r.UserAgent())
 		next(w, r)
+	}
+}
+
+type Joke struct {
+	Setup     string `json:"setup"`
+	Punchline string `json:"punchline"`
+}
+
+func jokeHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://official-joke-api.appspot.com/random_joke")
+	if err != nil {
+		http.Error(w, "Failed to get a joke", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var joke Joke
+	if err := json.NewDecoder(resp.Body).Decode(&joke); err != nil {
+		http.Error(w, "Failed to decode the joke", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "%s\n%s", joke.Setup, joke.Punchline)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
 	}
 }
 
@@ -40,6 +66,8 @@ func main() {
 			log.Printf("Error writing response: %v", err)
 		}
 	}))
+
+	http.HandleFunc("/joke", loggingMiddleware(jokeHandler))
 
 	// Server setup
 	server := &http.Server{
